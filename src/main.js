13abc3a700de1e461441c97353e2ba36e4aa1912
Rayng218 <manheing218@gmail.com>
2024-06-04 16:27:02 +0800

    basic setup

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap'
 const app = createApp(App)
 
 app.use(router)
